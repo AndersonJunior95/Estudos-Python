ea88ae337875b812c1e1ec00dc8547a3a9bf1250
Anderson Goncalves <andersongoncalves8419@gmail.com>
2025-08-27 18:07:32 -0300

    Adiciona exemplos de uso dos módulos math e random
    Inclui blocos de código comentados que demonstram funcionalidades básicas dos módulos  e .
    
    - O primeiro exemplo mostra como calcular a raiz quadrada de um número e arredondar o resultado para baixo ().
    - O segundo exemplo ilustra a geração de um número inteiro aleatório dentro de um intervalo específico com .

---
+++ Curso-em-Video/aula08/utilizando-modulos.py
@@ -0,0 +1,17 @@
+"""
+from math import sqrt, floor
+ou
+import math
+num = int(input("Digite um número: "))
+
+raiz = math.sqrt(num)
+print ("A raiz de {} é igual a {}".format(num, math.floor(raiz)))
+"""
+
+"""
+import random
+
+num = random.randint(1, 10)
+
+print (num)
+"""

0c79c4bb5f500472f1aba0a47c736574fad24148
Anderson Goncalves <andersongoncalves8419@gmail.com>
2025-08-27 19:42:37 -0300

    Implementa cálculo de salário de vendedor de carros
    Adiciona um script que calcula o salário final de um vendedor com base em um salário fixo e comissões.
    
    O algoritmo solicita ao usuário o nome do funcionário, o salário base e a quantidade de carros vendidos. O cálculo do salário final considera:
    - O salário base informado.
    - Uma comissão fixa por carro vendido.
    - Uma comissão de 5% sobre o valor total das vendas, que é calculado a partir de um valor médio pré-definido por carro.
    
    O resultado é exibido em um formato claro, detalhando o nome do funcionário, o salário base, a quantidade de carros e o salário final.

--- FACULDADE/aula01/Desafios/ex006.py
+++ FACULDADE/aula01/Desafios/ex006.py
@@ -1,10 +1,11 @@
 # Desafio proposto pelo professor: Uma revendedora de carros usados paga seus funcionarios(vendedores) um salário fixo por mês, mais uma comissão fixa para cada carro vendido e mais 5% do valor das vendas por ele efetuada. Escreva um algoritmo que leia o número de carros por ele vendido, total de suas vendas, o salário fixo que ele recebe por carro vendido, calcule e escreva o salário recebido pelo vendedor
 
+'''
+
 carrosVendidos = int(input("Digite a quantidade de carros vendidos: "))
 totalVendas = float(input("Digiteo o valor dos carros vendidos: "))
 salarioFixo = float(input("Digite o salário fixo: "))
 comissaoCarro = int(input("Digite a comissão fixa: "))
-
 
 comissaoTotal = comissaoCarro * carrosVendidos
 
@@ -13,3 +14,25 @@ percentual = totalVendas * 0.05
 salarioTotal = salarioFixo + percentual + comissaoTotal
 
 print ("O salário deu {:.2f}".format(salarioTotal))
+
+'''
+
+valFixo = 150
+valVendasCarro = 10000
+nome = ""
+qtdCarro = 0
+salario = 0
+com1 = 0
+com2 = 0
+
+nome = input("Digite o nome do funcionário. \n Nome: ")
+salario = float(input("Digite o salário desse funcionário \n Salário: "))
+qtdCarros = int(input("Digite quantos carros que o funcionário vendeu. \n Qunatidade: "))
+
+
+valVendas = qtdCarros * valVendasCarro
+com1 = qtdCarros * valFixo
+com2 = (qtdCarros * valVendasCarro) * 0.05
+salarioFinal = salario + com1 + com2
+
+print (f"Funcionário: {nome} \nSalário: {salario} \nQuantidade de carros vendidos: {qtdCarro} \n Salário Final: {salarioFinal}")
